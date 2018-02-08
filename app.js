77db6a120edfb39c04bbded2b6e4ccb808128c16
ahlstrominfo <mattias.ahlstrom@gmail.com>
2018-02-08 22:55:04 +0100

    Initial startup of this project. Code is a hack

---
+++ app.js
@@ -0,0 +1,58 @@
+var midi = require('midi');
+var config = require('./config.json');
+var Colors = require('./Colors.js');
+var input = new midi.input();
+var outputHUI = new midi.output();
+var inputPort = false;
+var outputHUIPort = false;
+
+for (var i = 0; i < input.getPortCount(); i++)
+{
+	if (input.getPortName(i).indexOf(config.input) !== -1) {
+		inputPort = i;
+		break;
+	}
+}
+for (var i = 0; i < outputHUI.getPortCount(); i++) {
+	if (outputHUI.getPortName(i).indexOf(config.hui) !== -1) {
+		outputHUIPort = i;
+		break;
+	}
+}
+
+if (inputPort === false || outputHUIPort === false) {
+	for (var i = 0; i < input.getPortCount(); i++)
+	{
+			console.log('Inputs available:', i, input.getPortName(i));
+	}
+
+	for (var i = 0; i < outputHUI.getPortCount(); i++)
+	{
+			console.log('Outputs available:', i, outputHUI.getPortName(i));
+	}
+
+	console.log('Update config.json to any of the above values');
+
+	process.exit()
+}
+
+outputHUI.openPort(outputHUIPort);
+var outputColors = new Colors(outputHUI);
+outputHUI.sendMessage([176+8, 0, 0]); // reset colors
+outputHUI.sendMessage([240,0,32,41,2,17,119,8,247]); // set factory template
+
+outputColors.setColor(8, 44, 58);
+outputColors.setColor(8, 24, 58);
+
+input.on('message', function(deltaTime, message) {
+  //console.log('m:' + message + ' d:' + deltaTime);
+	outputColors.setColor(8, 44, message[2]);
+	outputColors.setColor(8, 1, message[2]);
+	outputColors.setColor(8, 24, message[2]);
+});
+
+input.ignoreTypes(false, false, false);
+input.openPort(inputPort);
+
+console.log(outputHUI.getPortName(outputHUIPort), 'is the port that is showing fancy lights');
+console.log(input.getPortName(inputPort), 'is controlling Octatrack');
